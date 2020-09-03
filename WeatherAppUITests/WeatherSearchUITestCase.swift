5d5cc392fb3c22aae699b304bc2a7f12071acfde
Samar khaled <samar.khaled@gmail.com>
2020-09-03 17:30:16 +0200

    STORY-2 add UI test for empty search user input

---
+++ WeatherAppUITests/WeatherSearchUITestCase.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherAppUITests.swift
+//  WeatherAppUITests
+//
+//  Created by Samar Khaled on 9/3/20.
+//  Copyright © 2020 Samar Khaled. All rights reserved.
+//
+
+import XCTest
+
+class WeatherSearchUITestCase: XCTestCase {
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        app = XCUIApplication()
+        app.launch()
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        app = nil
+    }
+
+    func test_pressingGoButtonWithoutSearchText() throws {
+        let goButton = app/*@START_MENU_TOKEN@*/.staticTexts["Go!"]/*[[".buttons[\"Go!\"].staticTexts[\"Go!\"]",".staticTexts[\"Go!\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        goButton.tap()
+        let errorAlert = app.alerts["Error"].scrollViews.otherElements
+        XCTAssert(errorAlert.staticTexts["Please enter a search input first"].exists)
+        errorAlert.buttons["Ok"].tap()
+    }
+}
