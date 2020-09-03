cd162ce358083bd56ecd76870aaa7234a1e7ed9a
Samar khaled <samar.khaled@gmail.com>
2020-09-04 01:43:39 +0200

    STORY-2 add WeatherSearchModel  and noDataFound case

--- WeatherApp/WeatherSearch/WeatherSearch.swift
+++ WeatherApp/WeatherSearch/WeatherSearch.swift
@@ -12,6 +12,7 @@ enum WeatherSearchError: Error {
     case emptySearchInput
     case invalidSearchInput
     case errorRetriveData
+    case noDataFound
     var message: String {
         switch self {
         case .errorRetriveData:
@@ -20,6 +21,24 @@ enum WeatherSearchError: Error {
             return "Please enter a search input first"
         case .invalidSearchInput:
             return "Please enter a valid search input"
+        case .noDataFound:
+            return "City is not found"
         }
+    }
+}
+
+struct WeatherSearchModel {
+    var userInput: String
+
+    func isCity() -> Bool {
+        let numberRegEx  = ".*[0-9]+.*"
+        let testCase = NSPredicate(format: "SELF MATCHES %@", numberRegEx)
+        return !testCase.evaluate(with: userInput)
+    }
+
+    func isValidZipCode() -> Bool {
+        let postalcodeRegex = "^[0-9]{5}(-[0-9]{4})?$"
+        let pinPredicate = NSPredicate(format: "SELF MATCHES %@", postalcodeRegex)
+        return pinPredicate.evaluate(with: userInput) as Bool
     }
 }
