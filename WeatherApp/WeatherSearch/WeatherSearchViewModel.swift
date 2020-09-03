97b7de17cec9c6d7b03c16c83d225ce21f00d8f5
Samar khaled <samar.khaled@gmail.com>
2020-09-03 15:48:08 +0200

    STORY-2 adding WeatherSearchViewModel group WeatherSearch files

---
+++ WeatherApp/WeatherSearch/WeatherSearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherSearchViewModel.swift
+//  WeatherApp
+//
+//  Created by Samar Khaled on 9/3/20.
+//  Copyright © 2020 Samar Khaled. All rights reserved.
+//
+
+import Foundation
+
+class WeatherSearchViewModel {
+}
