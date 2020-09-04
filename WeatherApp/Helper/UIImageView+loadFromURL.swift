206641248e3afc6f3e26e65848bf9761e26929a9
Samar khaled <samar.khaled@gmail.com>
2020-09-04 19:39:33 +0200

    STORY-3  add UIImageView extension to load from url

---
+++ WeatherApp/Helper/UIImageView+loadFromURL.swift
@@ -0,0 +1,24 @@
+//
+//  UIImageView+loadFromURL.swift
+//  WeatherApp
+//
+//  Created by Samar Khaled on 9/4/20.
+//  Copyright © 2020 Samar Khaled. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL?) {
+        guard let url = url else { return }
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
